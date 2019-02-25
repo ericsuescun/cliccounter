e86a85d332975a718134d835b373f677b2a7a52e
Eric Suescun <edsuescun@gmail.com>
2019-02-25 15:39:37 -0500

    Uploading solution.rb with counter working by pressin button q

---
+++ solution.rb
@@ -0,0 +1,16 @@
+require 'sinatra'
+
+get '/' do
+
+	if !params[:incr] || params[:incr].empty?	#Unless I pass something through the URL, it will show Hello World
+  		@count = 0.to_i
+  	else
+  		@count = @count.to_i + params[:incr].to_i
+  	end
+
+	erb :form
+end
+
+post '/' do
+	redirect to('/?incr=' + (params[:var].to_i + 1).to_s)
+end
